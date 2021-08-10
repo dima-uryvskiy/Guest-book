310e14c54f12022f391f83135dbbf1e492b11932
dima-uryvskiy <dima-uryvskiyy@rambler.ru>
2021-08-10 22:47:38 +0300

    Add admin panel

---
+++ src/Controller/Admin/ConferenceCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Conference;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+
+class ConferenceCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Conference::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+       yield IdField::new('id');
+       yield AssociationField::new('comments');
+       yield TextField::new('city');
+       yield TextField::new('year');
+       yield BooleanField::new('isInternational');
+
+    }
+}
