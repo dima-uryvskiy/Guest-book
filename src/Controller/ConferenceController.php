c885c4333e69e3cc6095853250cd133e5668fac3
dima-uryvskiy <dima-uryvskiyy@rambler.ru>
2021-09-13 22:57:31 +0300

    Update views conference

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -52,6 +52,7 @@ class ConferenceController extends AbstractController
 
         return new Response($this->twig->render('conference/show.html.twig', [
             'conference' => $conference,
+            'conferences' => $conferenceRepository->findAll(),
             'comments'   => $paginator,
             'previous'   => $offset - CommentRepository::PAGINATOR_PER_PAGE,
             'next'       => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
